6b6492fd1b3f6b3afbf0968a27d3e295060ef8dd
Ryan Prana <ryanprana555@gmail.com>
2023-12-09 22:15:51 +0800

    edit db config

--- config/db.config.js
+++ config/db.config.js
@@ -2,11 +2,15 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_PRODUCTION_DATABASE,
-  process.env.DB_PRODUCTION_USERNAME,
-  process.env.DB_PRODUCTION_PASSWORD,
+  // process.env.DB_PRODUCTION_DATABASE,
+  // process.env.DB_PRODUCTION_USERNAME,
+  // process.env.DB_PRODUCTION_PASSWORD,
   {
+    username: process.env.DB_PRODUCTION_USERNAME,
+    password: process.env.DB_PRODUCTION_PASSWORD,
+    database: process.env.DB_PRODUCTION_DATABASE,
     host: process.env.DB_PRODUCTION_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     // logging: false,
   }
